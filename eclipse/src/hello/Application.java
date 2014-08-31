ba13ed62a159f8172c52057b8d378d9c113cb623
pratz.nud@gmail.com <pratz.nud@gmail.com@9b72897c-b9da-664f-5804-9c30fd70bfc4>
2014-08-31 14:36:03 +0000

    Import SpringBatchProcess

---
+++ eclipse/src/hello/Application.java
@@ -0,0 +1,41 @@
+package hello;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+@ComponentScan
+@EnableAutoConfiguration
+@ImportResource({"database-config.xml"})
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+        
+        List<Person> results = ctx.getBean(JdbcTemplate.class).query("SELECT first_name, last_name FROM people", new RowMapper<Person>() {
+            @Override
+            public Person mapRow(ResultSet rs, int row) throws SQLException {
+                return new Person(rs.getString(1), rs.getString(2));
+            }
+        });
+
+        for (Person person : results) {
+            System.out.println("Found <" + person + "> in the database.");
+        }
+    }
+
+}
