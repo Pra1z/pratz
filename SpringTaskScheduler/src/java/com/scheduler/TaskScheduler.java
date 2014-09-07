b9208e16865cd3c144785e24a889c8ecc54cb563
pratz.nud@gmail.com <pratz.nud@gmail.com@9b72897c-b9da-664f-5804-9c30fd70bfc4>
2014-09-07 00:02:13 +0000

    create SpringTaskScheduler on web container

---
+++ SpringTaskScheduler/src/java/com/scheduler/TaskScheduler.java
@@ -0,0 +1,14 @@
+
+package com.scheduler;
+
+import java.util.Date;
+ 
+public class TaskScheduler
+{
+   
+    public void run()
+    {
+       
+        System.out.println("Method executed at every 5 seconds. Current time is :: "+ new Date());
+    }
+}
